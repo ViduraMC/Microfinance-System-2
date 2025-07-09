3766c8eb084999bf3306b84c922644b96253d878
“[Vidura <Vidura1159@gmail.com>
2025-07-09 11:15:06 +0530

    Vidura- Login upload

---
+++ Login/frontend/src/Components/DeathaidHomePage/DeathaidHomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function DeathaidHomePage() {
+  return (
+    <div>
+      <h1>Welcome to Deathaid Home Page</h1>
+      <p>You are logged in!</p>
+    </div>
+  );
+}
+
+export default DeathaidHomePage; 
